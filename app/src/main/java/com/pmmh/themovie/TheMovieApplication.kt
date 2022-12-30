673b48b7d94b7327eeeb4a79dd524d8c9c8c8bf0
PhyoeMaung137 <phyoemaungmaung.htun@ascendcorp.com>
2022-12-30 22:15:58 +0630

    adding require dependencies

---
+++ app/src/main/java/com/pmmh/themovie/TheMovieApplication.kt
@@ -0,0 +1,17 @@
+package com.pmmh.themovie
+
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TheMovieApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
+    companion object {
+        lateinit var context: Context
+    }
+}
